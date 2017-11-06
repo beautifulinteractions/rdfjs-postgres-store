67dde6642eb0e063139e4d04de4d2871e32485b3
Jacopo Scazzosi <me@jacoscaz.com>
2017-11-06 22:12:41 +0000

    simpler schema, batch import

--- test.js
+++ test.js
@@ -26,43 +26,38 @@ function streamToArray(readStream) {
   await store.createTables();
 
   const quads = [];
-  for (let i = 0; i < 10000; i++) {
+  for (let i = 0; i < 20000; i++) {
     quads.push(factory.quad(
       factory.namedNode('http://ex.com/s' + i),
-      factory.namedNode('http://ex.com/p' + Math.floor(i % 4)),
+      factory.namedNode('http://ex.com/p' + i),
       factory.literal('' + i, 'http://www.w3.org/2001/XMLSchema#integer'),
-      factory.namedNode('http://ex.com/g')
+      factory.namedNode('http://ex.com/g' + i)
     ));
   }
 
   const preImport = Date.now();
-  await asynctools.onEvent(store.import(new ArrayIterator(quads)), 'end');
+  await asynctools.onEvent(store.import(new ArrayIterator(quads), { batch: 10 }), 'end');
   const postImport = Date.now();
 
   console.log('IMPORT TIME', (postImport - preImport) / 1000);
 
+  const count = await store.estimate(null, null, [
+    { test: 'gt', comparate: factory.literal('5', 'http://www.w3.org/2001/XMLSchema#integer') },
+    { test: 'lt', comparate: factory.literal('10', 'http://www.w3.org/2001/XMLSchema#integer') }
+  ]);
+  const found = await streamToArray(store.match(null, null, [
+    { test: 'gt', comparate: factory.literal('5', 'http://www.w3.org/2001/XMLSchema#integer') },
+    { test: 'lt', comparate: factory.literal('10', 'http://www.w3.org/2001/XMLSchema#integer') }
+  ]));
+
+  console.log('COUNT', count, found.length);
+
   const preRemove = Date.now();
-  await asynctools.onEvent(store.removeMatches(), 'end');
+  await asynctools.onEvent(store.remove(new ArrayIterator(quads)), 'end');
   const postRemove = Date.now();
 
   console.log('REMOVE TIME', (postRemove - preRemove) / 1000);
 
-  // const count = await store.estimate(null, null, [
-  //   { test: 'gt', comparate: factory.literal('5', 'http://www.w3.org/2001/XMLSchema#integer') },
-  //   { test: 'lt', comparate: factory.literal('10', 'http://www.w3.org/2001/XMLSchema#integer') }
-  // ]);
-  // const found = await streamToArray(store.match(null, null, [
-  //   { test: 'gt', comparate: factory.literal('5', 'http://www.w3.org/2001/XMLSchema#integer') },
-  //   { test: 'lt', comparate: factory.literal('10', 'http://www.w3.org/2001/XMLSchema#integer') }
-  // ]));
-  //
-  // console.log('COUNT', count, found.length);
-  //
-  // await asynctools.onEvent(store.removeMatches(null, factory.namedNode('http://ex.com/p1')), 'end');
-  //
-  // console.log('COUNT2', await store.estimate());
-  //
-  // console.log('Done');
 
   await store.close();
 
